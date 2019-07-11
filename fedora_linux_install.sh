510d93b5bb328ef6377a5a71867567c0aefacd07
Jonas Andersson <jonananas@gmail.com>
2019-07-11 21:29:49 +0200

    using predefined $USER

--- fedora_linux_install.sh
+++ fedora_linux_install.sh
@@ -6,9 +6,8 @@ git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 
 # Setup zsh and oh-my-zsh
-USERNAME=username
 sudo dnf install zsh
-sudo usermod -s /bin/zsh $USERNAME
+sudo usermod -s /bin/zsh $USER
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # Logout and login again to have zsh take effect
 sudo dnf install fasd
